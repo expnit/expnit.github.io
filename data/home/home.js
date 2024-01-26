b37ee42d60df15a21aac2505bbcf42c6bc3cc9e5
Pruthviraj <expnit@outlook.com>
2024-01-26 16:18:29 +0530

    added some data and favicon

---
+++ data/home/home.js
@@ -0,0 +1,46 @@
+const technology = [
+    {
+        title: "Linux",
+        "data": "As the digital landscape hurtles towards new horizons, Linux remains at the forefront of innovation. Its adaptability and the relentless drive of its community make it well-positioned to tackle the challenges of tomorrow, from artificial intelligence and the Internet of Things to the exploration of new frontiers in quantum computing and beyond.",
+    },
+    {
+        "title": "Programming",
+        "data": "So, programming is not just about clicking buttons or dragging icons. It's about speaking the language of machines, crafting instructions that dance within their silicon minds. It's about wielding the power of logic and abstraction to shape the digital world to our will. As the bridge between human and machine, programming languages empower us to push the boundaries of what's possible, one line of code at a time."
+    },
+    {
+        "title": "Artificial Intelligence",
+        "data": "Artificial intelligence (AI) has transcended its sci-fi origins to become a transformative force shaping our world. It's no longer just about sentient robots; it's about algorithms mimicking human intelligence to solve complex problems in diverse fields."
+    },
+    {
+        "title": "Frameworks",
+        "data": "Imagine building a house. Bricks and mortar alone won't suffice; you need a framework, a pre-existing structure that guides construction, ensures stability, and accelerates the process. In the realm of software development, frameworks fulfill a similar role."
+    }
+];
+
+
+const sanatan = [
+    {
+        title: "Explore Sanatan",
+        "data": "Explore Sanatana"
+    },
+    {
+        title: "Scientific Study of Sanatan",
+    },
+    {
+        title: "inspirational Sanatan",
+    }
+];
+
+const news = [
+    {
+        title: "International",
+    },
+    {
+        title: "National",
+    },
+    {
+        title: "Premium",
+    }
+];
+
+export { technology, sanatan }
